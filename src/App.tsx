1ce3b8dc9c6abdf188becbe99a7334b4513af8cd
ZacharyNWilliams <zacharynwilliams9@gmail.com>
2023-02-11 12:53:40 -0800

    done

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { MenuList } from './components/MenuList';
+import { Sidebar } from './components/Sidebar';
+import OrderContextProvider from './context/OrderContextProvider';
+
+function App() {
+  return (
+    <div className="App">
+      <OrderContextProvider>
+      <Header />
+      <MenuList />
+      <Sidebar />
+      </OrderContextProvider>
+    </div>
+  );
+}
+
+export default App;
